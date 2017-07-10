5c4cd468769fe1be65fe9614fcf9f1c0e767e722
Peter <PeterGrantBue@gmail.com>
2017-07-10 14:34:06 -0700

    Contact form functionality almost done

--- js/contact.js
+++ js/contact.js
@@ -3,15 +3,29 @@ $(document).ready(function() {
     e.preventDefault();
 
     var inputs = $("input[type='text']");
-    var dictionary = [];
+    var dictionary = {};
     inputs.each(function(index, element) {
+      $(element).removeClass("form-validation");
       dictionary[$(element).attr('name')] = $(element).val();
     });
 
     $.post($(this).attr('action'), dictionary, function(data) {
-      alert('SUCCESS!');
+      var button = $("button[type='submit']");
+      button.css('display', 'none');
+      button.siblings('p').text('Thank you for your interest in the Thin Skin Movie, expect updates soon!');
+      inputs.each(function(index, element) {
+        $(element).addClass("form-success");
+      });
     }).fail(function(data) {
-      alert('FAILURE');
+      var errors = $.parseJSON(data.responseText)["errors"];
+      $.each(errors, function(index, element) {
+        var input = $("input[name='" + index + "']");
+        var sibs = input.siblings("span");
+        sibs.text(element);
+        input.removeClass("form-success");
+        input.addClass("form-validation");
+        input.siblings("span").val(element)
+      });
     });
   });
 });
--- scripts/contact.php
+++ scripts/contact.php
@@ -1,3 +1,91 @@
 <?php
   ini_set('display_errors', 1);
   error_reporting(E_ALL);
+
+  $dbh = new PDO('mysql:host=localhost; dbname=ThinSkin_Subscribers', "ThinDB_Admin", "Shotgun7");
+
+
+
+  $input = $_POST;
+
+  $data = array();
+  $errors = array();
+  $debug = array();
+
+  if(empty($input['fName']))
+        $errors['fName'] = 'First Name is required.';
+
+  if(empty($input['lName']))
+        $errors['lName'] = 'Last Name is required.';
+
+  if(empty($input['email'])) {
+    $errors['email'] = 'Email Address is required.';
+  } else if(!filter_var($input['email'], FILTER_VALIDATE_EMAIL)){
+    $errors['email'] = 'A valid email is required';
+  }
+
+  checkForErrors($errors);
+  $input = cleanInputs($input);
+
+  try {
+        $stmt = $dbh->prepare("INSERT INTO Subscribers (FIRST_NAME, LAST_NAME, EMAIL, ENTRY_DATE)
+        VALUES (:first, :last, :email, :date)");
+        $stmt->execute(
+          Array
+          (
+            "date" => gmdate("Y-m-d H:i:s"),
+            "first" => $input['fName'],
+            "last" => $input['lName'],
+            "email" => $input["email"]
+          )
+        );
+    } catch (PDOException $ex) {
+      $debug["errors"]["PDO insert statement failure"];
+      overrideResponse($debug);
+    }
+
+  sendResponse($debug);
+
+  function checkForErrors($errors) {
+      if(!empty($errors)) {
+          $data['success'] = false;
+          $data['errors'] = $errors;
+          overrideResponse($data, 400);
+      }
+  }
+
+  function sendResponse($data, $status = 200) {
+      header("HTTP/1.1 " . $status . " " . requestStatus($status));
+      echo json_encode($data);
+  }
+
+  function overrideResponse($data, $status = 400) {
+      header("HTTP/1.1 " . $status . " " . requestStatus($status));
+      echo json_encode($data);
+      die();
+  }
+
+  function cleanInputs($data) {
+        $clean_input = Array();
+        if (is_array($data)) {
+            foreach ($data as $k => $v) {
+                $clean_input[$k] = cleanInputs($v);
+            }
+        } else {
+            $clean_input = trim(strip_tags($data));
+        }
+        return $clean_input;
+    }
+
+    function requestStatus($code) {
+        $status = array(
+            200 => 'OK',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            410 => 'Gone',
+            500 => 'Internal Server Error',
+        );
+        return ($status[$code]) ? $status[$code] : $status[500];
+    }
